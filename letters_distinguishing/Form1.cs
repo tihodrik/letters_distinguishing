99d2c4ab4ac63ea20a712b4d5da14143b98a3d14
Darya Tikhomirova <tihodrik@gmail.com>
2017-10-11 05:31:04 +0300

    finished

--- letters_distinguishing/Form1.cs
+++ letters_distinguishing/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,181 @@ namespace letters_distinguishing
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.TextBox result_tb;
+        private System.Windows.Forms.GroupBox template;
+
+        // control[number] -> Pair(seq of 0s and 1s, result)
+        private Dictionary<List<int>, string> _X;
+        private List<List<int>> _D;
+
+        public int pointsX { get; private set; }
+        public int pointsY { get; private set; }
+
+        private List<Neuron> perseptron;
+
         public Form1()
         {
+            pointsX = 3;
+            pointsY = 4;
+
             InitializeComponent();
+            CreateFormElements(pointsX, pointsY);
+
+            GetKnowledgeBase();
+            PerseptronInitialization();
+
+            int steps = Educate();
+        }
+
+        private void CreateFormElements(int pointsX, int pointsY)
+        {
+            int x0 = 15, y0 = 15;
+            int size = 20;
+
+            template = new GroupBox
+            {
+                Location = new System.Drawing.Point(10, 10),
+                Width = x0 * 2 + size * pointsX - x0 / 2,
+                Height = y0 * 2 + size * pointsY - y0 / 2,
+                Name = "template"
+            };
+
+            List<CheckBox> number = new List<CheckBox>();
+
+            for (int i = 0; i < pointsY; i++)
+            {
+                for (int j = 0; j < pointsX; j++)
+                {
+                    number.Add(new CheckBox
+                    {
+                        Location = new System.Drawing.Point(x0 + j * size, y0 + i * size),
+                        Width = size,
+                        Height = size
+                    });
+
+                    template.Controls.Add(number.Last());
+                }
+            }
+            this.Controls.Add(template);
+
+            Button check_btn = new Button
+            {
+                Text = "Check",
+                Name = "check_btn",
+                Width = template.Width,
+                Location = new System.Drawing.Point(template.Location.X, template.Location.Y + template.Height + 10)
+            };
+            this.Controls.Add(check_btn);
+
+            check_btn.Click += new System.EventHandler(this.Check_btn_click);
+
+            result_tb = new TextBox
+            {
+                Name = "result_tb",
+                Width = template.Width,
+                ReadOnly = true,
+                TextAlign = HorizontalAlignment.Center,
+                Font = new System.Drawing.Font("Times New Roman", 10.0F),
+                Location = new System.Drawing.Point(check_btn.Location.X, check_btn.Location.Y + check_btn.Height + 10),
+            };
+            this.Controls.Add(result_tb);
+
+            this.Height = 80 + template.Height + check_btn.Height + result_tb.Height;
+            this.Width = template.Width + 10;
+        }
+
+        private void Check_btn_click(object sender, EventArgs e)
+        {
+            result_tb.Clear();
+            for (int i = 0; i < 33; i++)
+            {
+                perseptron[i].SetX(template);
+                if (perseptron[i].Y == 1)
+                    result_tb.Text += perseptron[i].letter;
+            }
+        }
+
+        private void PerseptronInitialization()
+        {
+            Neuron n;
+            perseptron = new List<Neuron>();
+            
+            for (int i = 0; i < 33; i++)
+            {
+                n = new Neuron(pointsX * pointsY + 1, _X.ElementAt(i).Value);
+                perseptron.Add(n);
+            }
+        }
+
+        private void GetKnowledgeBase()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.DefaultExt = "txt";
+            dialog.Filter = "Text files (*.txt)|*txt";
+            dialog.InitialDirectory = Environment.CurrentDirectory;
+            dialog.ShowDialog();
+
+            StreamReader f;
+
+            if (dialog.FileName.Length != 0)
+                f = new StreamReader(dialog.FileName, Encoding.Default);
+            else
+                return;
+
+
+            List<int> _x;
+            List<int> _d;
+            _X = new Dictionary<List<int>, string>();
+            _D = new List<List<int>>();
+
+            for (int i = 0; i < 33 || f.EndOfStream == false; i++)
+            {
+                string line = f.ReadLine();
+
+                string[] tmp = line.Split(' ');
+
+                _x = new List<int>();
+                for (int j = 0; j < 12; j++)
+                {
+                    _x.Add(int.Parse(tmp[j]));
+                }
+                _X.Add(_x, tmp.Last());
+
+                _d = new List<int>();
+                for (int j = 0; j < 33; j++)
+                {
+                    if (j == i)
+                        _d.Add(1);
+                    else
+                        _d.Add(0);
+                }
+                _D.Add(_d);
+            }
+
+            f.Close();
+        }
+
+        private int Educate()
+        {
+            bool wrong_answ;
+            int count = 0;
+
+            do
+            {
+                count++;
+                wrong_answ = false;
+                for (int l = 0; l < 33; l++)
+                {
+                    for (int i = 0; i < 33; i++)
+                    {
+                        perseptron[i].X = _X.ElementAt(l).Key;
+                        perseptron[i].D = _D[l][i];
+                        wrong_answ |= perseptron[i].CorrectW();
+                    }
+                }
+            } while (wrong_answ);
+
+            return count;        
         }
     }
 }
---
+++ letters_distinguishing/Neuron.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace letters_distinguishing
+{
+    class Neuron
+    {
+        public List<int> x;
+        public List<int> X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                for (int j = 1; j < x.Count; j++)
+                {
+                    x[j] = value[j - 1];
+                }
+            }
+        }
+        public int D {
+            get; set;
+        }
+
+        public int Y
+        {
+            get
+            {
+                if (S >= 0)
+                    return 1;
+                return 0;
+            }
+        }
+        private List<double> w;
+
+        private double s;
+        private double S
+        {
+            get
+            {
+                s = 0;
+                for (int j = 0; j < x.Count; j++)
+                {
+                    s += x[j] * w[j];
+                }
+                return s;
+            }
+        }
+        public int E
+        {
+            get
+            {
+                return D - Y;
+            }
+        }
+
+        public string letter;
+          
+
+        public Neuron(int entries, string _letter)
+        {
+            letter = _letter;
+
+            x = new List<int>(entries);
+            w = new List<double>(entries);
+
+            for (int j = 0; j < entries; j++)
+            { 
+                x.Add(0);
+
+                Random rand = new Random();
+                w.Add(rand.Next(-5, 5) + rand.NextDouble());
+            }
+
+            x[0] = 1;
+        }
+
+        public void SetX(GroupBox template)
+        {
+            for (int i = 1; i < x.Count; i++)
+            {
+                if ((template.Controls[i-1] as CheckBox).Checked)
+                    x[i] = 1;
+                else
+                    x[i] = 0;
+            }
+        }
+
+        /// <summary>
+        /// Correct w for each entry
+        /// </summary>
+        /// <returns>true if correction has been done</returns>
+        public bool CorrectW()
+        {
+            int err = E;
+
+            if (err == 0)
+                return false;
+
+            for (int j = 0; j < w.Count; j++)
+            {
+                w[j] += err * x[j];
+            }
+            return true;
+        }
+    }
+}
